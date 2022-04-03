719dbf9c9bed205fa59f9a98ff5907100e09b392
Matheus <matheus.oliveira110101@gmail.com>
2022-04-03 12:07:45 -0300

    componente Page

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Page from './views/Page';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Tela inicial do App</Text>
+      <Page empresa='Celerus' />
       <StatusBar style="auto" />
     </View>
   );
