076c3a8e67a2233bbe81ddc5ce9b14031dfa7296
Matheus <matheus.oliveira110101@gmail.com>
2022-04-04 20:16:25 -0300

    inclusao do Navigation

--- App.js
+++ App.js
@@ -2,14 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import Page from './views/Page';
 import {css} from './assets/css/Css';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './views/Home';
 
 export default function App() {
+
+  const Stack = createStackNavigator();
   return (
-    <View style={css.container}>
-      <Text>Tela inicial do App</Text>
-      <Page empresa='Celerus' />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
